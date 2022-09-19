0d83e6c9ad2a6ef1df6f8fe6721887ef708de42a
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2022-09-19 17:27:16 -0300

    Clonagem concluida

---
+++ script.js
@@ -0,0 +1,114 @@
+const cont = document.querySelector("#a")
+        const cont2 = document.querySelector("#b")
+        const setacima = document.querySelector("#setacima")
+        const setabaixo = document.querySelector("#setabaixo")
+        cont2.style.display = "none"
+        cont.addEventListener("click", function(){
+            if(cont2.style.display == "none"){
+                cont2.style.display="block"
+                setacima.classList.remove("invisivel")
+                setabaixo.classList.add("invisivel")
+                cont.style.backgroundColor = "rgb(232, 229, 229)"
+                cont.style.textDecoration= "underline"
+                
+
+            }else{
+                cont2.style.display = "none"
+                setacima.classList.add("invisivel")
+                setabaixo.classList.remove("invisivel")
+                cont.style.backgroundColor = "rgb(232, 229, 229)"
+                cont.style.textDecoration= "underline"
+    
+
+            }
+        })
+        //se eu clicar em qualquer elemento do body q n seja o cont ele tira op underline da sec e coloca o background em white
+        document.body.addEventListener("click",function(e){
+                if(e.target != cont){
+                        cont.style.backgroundColor = "white"
+                        cont.style.textDecoration = "none"
+                }
+        })
+        const cont3 = document.querySelector("#c")
+        const cont4 = document.querySelector("#d")
+        const setacima1 = document.querySelector("#setacimaa")
+        const setabaixo2 = document.querySelector("#setabaixoo")
+        cont4.style.display = "none"
+        cont3.addEventListener("click", function(){
+            if(cont4.style.display == "none"){
+                cont4.style.display="block"
+                setacima1.classList.remove("invisivel")
+                setabaixo2.classList.add("invisivel")
+                cont3.style.backgroundColor = "rgb(232, 229, 229)"
+                cont3.style.textDecoration= "underline"
+
+            }else{
+                cont4.style.display = "none"
+                setacima1.classList.add("invisivel")
+                setabaixo2.classList.remove("invisivel")
+                cont3.style.backgroundColor = "rgb(232, 229, 229)"
+                cont3.style.textDecoration= "underline"
+
+            }
+        })
+        document.body.addEventListener("click",function(e){
+            if(e.target != cont3){
+                cont3.style.textDecoration = "none"
+                cont3.style.backgroundColor = "white"
+            }
+        })
+
+        const cont5 = document.querySelector("#e")
+        const cont6 = document.querySelector("#f")
+        const setacima3 = document.querySelector("#setacimaaa")
+        const setabaixo4 = document.querySelector("#setabaixooo")
+        cont6.style.display = "none"
+        cont5.addEventListener("click",function(){
+            if(cont6.style.display == "none"){
+                cont6.style.display = "block"
+                setacima3.classList.remove("invisivel")
+                setabaixo4.classList.add("invisivel")
+                cont5.style.backgroundColor = "rgb(232, 229, 229)"
+                cont5.style.textDecoration= "underline"
+            }
+            else{
+                cont6.style.display = "none"
+                setacima3.classList.add("invivisivel")
+                setabaixo4.classList.remove("invisivel")
+                cont5.style.backgroundColor = "rgb(232, 229, 229)"
+                cont5.style.textDecoration= "underline"
+            }
+        })
+        document.body.addEventListener("click", function(e){
+            if(e.target != cont5){
+                cont5.style.backgroundColor = "white"
+                cont5.style.textDecoration = "none"
+            }
+        })
+
+        const cont7 = document.querySelector("#g")
+        const cont8 = document.querySelector("#h")
+        const setacima5 = document.querySelector("#setacimaaaa")
+        const setabaixo6 = document.querySelector("#setabaixoooo")
+        cont8.style.display = "none"
+        cont7.addEventListener("click", function(){
+            if(cont8.style.display == "none"){
+                cont8.style.display = "block"
+                setabaixo6.classList.add("invisivel")
+                setacima5.classList.remove("invisivel")
+                cont7.style.backgroundColor = "rgb(232, 229, 229)"
+                cont7.style.textDecoration = "underline"
+            }else{
+                cont8.style.display = "none"
+                setabaixo6.classList.remove("invisivel")
+                setacima5.classList.add("invisivel")
+                cont7.style.backgroundColor = "rgb(232, 229, 229)"
+                cont7.style.textDecoration = "underline"
+            }
+        })
+        document.body.addEventListener("click", function(e){
+            if(e.target != cont7){
+                cont7.style.backgroundColor = "white"
+                cont7.style.textDecoration = "none"
+            }
+        })
